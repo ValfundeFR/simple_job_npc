5028ebbdaf514cf7b86c8e483d220d15cc195e2e
Valfunde <valfunde@gmx.fr>
2017-08-11 18:28:56 +0200

    Update job_npc_config.lua

--- lua/autorun/job_npc_config.lua
+++ lua/autorun/job_npc_config.lua
@@ -1,6 +1,6 @@
 --[[-------------------------------------------------------------------------
 Script by Valfunde | Version 1.0
-If you have any problems, don't hesitate to make github issue
+If you have any problems, don't hesitate to contact me or add me on steam (Valfunde *Banna Army) but write a comment before adding me
 This is configuration file.
 ---------------------------------------------------------------------------]]
 
